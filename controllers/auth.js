f3cf3506b286a1b79c4f31e3afb6b6412e34b7ba
Md Darudul Islam <darudulislam@gmail.com>
2022-11-09 00:19:44 +0800

    user authorization added

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,12 +30,10 @@ export const login = async (req, res, next) => {
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
         if (!isPasswordCorrect) return next(createError(400, "Wrong password or username!"));
 
-        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT, { expiresIn: '1d' });
+
         const { password, isAdmin, ...otherDetails } = user._doc;
-        res
-            .cookie("access_token", token, {
-                httpOnly: true,
-            })
+        res.cookie("access_token", token, { httpOnly: true })
             .status(200)
             .json({ ...otherDetails })
     } catch (err) {
