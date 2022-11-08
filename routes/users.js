aa2eada39f37674436adb9990538b6a76278c085
Md Darudul Islam <darudulislam@gmail.com>
2022-11-09 00:39:34 +0800

    hotel athorization added

--- routes/users.js
+++ routes/users.js
@@ -15,12 +15,13 @@ const router = express.Router();
 // })
 
 // UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 // DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 // GET
+router.get("/:id", verifyUser, deleteUser);
 // GET ALL
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router
